09facfc391c45f68f8b8191048a4bfabbdeaa9be
Aatika Khan <aatikakhan869@gmail.com>
2022-04-09 23:58:41 +0530

    add seach bar design

--- lib/main.dart
+++ lib/main.dart
@@ -32,10 +32,61 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(backgroundColor: Colors.white,
-        title: Text(widget.title,style: const TextStyle(color: Colors.black),),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            backgroundColor: Colors.white,
+            floating: true,
+            pinned: true,
+            snap: false,
+            centerTitle: false,
+            title: Text(
+              widget.title,
+              style: const TextStyle(color: Colors.black),
+            ),
+            bottom: AppBar(
+              backgroundColor: Colors.white,
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * .76,
+                    height: 40,
+                    child: const TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(5.0)),
+                            borderSide: BorderSide(color: Colors.blue)),
+                        contentPadding: EdgeInsets.all(8),
+                        hintText: 'Search e.g. "fruits"',
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.search, color: Colors.black),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              SizedBox(
+                height: MediaQuery.of(context).size.height,
+                child: const Center(
+                  child: Text('Not found'),
+                ),
+              ),
+              Container(
+                height: 1000,
+              ),
+            ]),
+          ),
+        ],
       ),
-      body: Center(child: Container()),
     );
   }
 }

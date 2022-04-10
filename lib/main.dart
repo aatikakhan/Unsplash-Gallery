e88acc07221c1f044ca6645c4fddfebbc4bdb7df
Aatika Khan <aatikakhan869@gmail.com>
2022-04-11 00:39:43 +0530

    add provider to base

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:ungallery/data_provider.dart';
+
+import 'screens.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider<DataProvider>(
+          create: (_) => DataProvider(),
+        ),
+      ],
+      child: Builder(
+        builder: (context) {
+          return const MyApp();
+        },
+      ),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -16,77 +33,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Unsplash'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            backgroundColor: Colors.white,
-            floating: true,
-            pinned: true,
-            snap: false,
-            centerTitle: false,
-            title: Text(
-              widget.title,
-              style: const TextStyle(color: Colors.black),
-            ),
-            bottom: AppBar(
-              backgroundColor: Colors.white,
-              title: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * .76,
-                    height: 40,
-                    child: const TextField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(5.0)),
-                            borderSide: BorderSide(color: Colors.blue)),
-                        contentPadding: EdgeInsets.all(8),
-                        hintText: 'Search e.g. "fruits"',
-                      ),
-                    ),
-                  ),
-                  const SizedBox(width: 8),
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.search, color: Colors.black),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          SliverList(
-            delegate: SliverChildListDelegate([
-              SizedBox(
-                height: MediaQuery.of(context).size.height,
-                child: const Center(
-                  child: Text('Not found'),
-                ),
-              ),
-              Container(
-                height: 1000,
-              ),
-            ]),
-          ),
-        ],
-      ),
     );
   }
 }

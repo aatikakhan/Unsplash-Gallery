1a35128161ece04a00279b1a8b828b468e88c866
Aatika Khan <aatikakhan869@gmail.com>
2022-04-11 00:40:18 +0530

    get data using provider

---
+++ lib/data_provider.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
+
+import 'constants.dart';
+import 'data_model.dart';
+
+class DataProvider with ChangeNotifier {
+  List<DataModel>? data;
+  String? status = 'hold on';
+
+  Future<List<DataModel>?> getData() async {
+    Response res = await http
+        .get(Uri.parse('https://api.unsplash.com/photos/?client_id=$id'));
+    if (res.statusCode == 200) {
+      List<dynamic> body = jsonDecode(res.body);
+      data = body.map((item) => DataModel.fromJson(item)).toList();
+      notifyListeners();
+      return data;
+    } else {
+      status = res.reasonPhrase;
+      throw "Unable to retrieve data.";
+    }
+  }
+}

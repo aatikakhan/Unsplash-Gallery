dca7b34c89147ecca936a6f876203b8587a2c329
Aatika Khan <aatikakhan869@gmail.com>
2022-04-13 00:26:27 +0530

    do query based get request

--- lib/data_provider.dart
+++ lib/data_provider.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:http/http.dart';
 
 import 'constants.dart';
 import 'data_model.dart';
@@ -10,13 +9,28 @@ import 'data_model.dart';
 class DataProvider with ChangeNotifier {
   List<DataModel>? data;
   String? status = 'hold on';
+  DataModel? dat;
+  String? searchQuery = 'star-wars';
 
   Future<List<DataModel>?> getData() async {
-    Response res = await http
-        .get(Uri.parse('https://api.unsplash.com/photos/?client_id=$id'));
+    String url = "https://api.unsplash.com/search/photos/";
+
+    Map<String, String> qParams = {
+      'client_id': id,
+      'query': searchQuery!,
+    };
+
+    Uri uri = Uri.parse(url).replace(queryParameters: qParams);
+
+    final res = await http.get(uri);
+
     if (res.statusCode == 200) {
-      List<dynamic> body = jsonDecode(res.body);
-      data = body.map((item) => DataModel.fromJson(item)).toList();
+      var decodedJson = json.decode(res.body);
+      List jsondata = decodedJson['results'];
+      data = jsondata
+          .map((item) => DataModel.fromJson(item as Map<String, dynamic>))
+          .toList();
+
       notifyListeners();
       return data;
     } else {
